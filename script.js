8e5000b69cfbcd97bae5ec73fa8b88735558e84b
AvihayR <avihayrabi@gmail.com>
2023-03-16 15:08:36 -0700

    Add modal

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+//Query selectors
+const modal = document.querySelector('.modal');
+const modalBtn = document.getElementById('modal-btn');
+const closeBtn = document.querySelector('.close');
+
+//Modal
+modalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', () => {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+});
+
 function createBook(title, author, pages, read) {
   this.title = title;
   this.author = author;
