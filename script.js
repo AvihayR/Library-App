35a24da1e41bad07c2bce9b16be657578421ffff
AvihayR <avihayrabi@gmail.com>
2023-03-18 04:08:44 -0700

    Add display books logic

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const modal = document.querySelector('.modal');
 const modalBtn = document.getElementById('modal-btn');
 const closeBtn = document.querySelector('.close');
+const gridContainer = document.querySelector('.grid-container');
 
 //Modal
 modalBtn.addEventListener('click', () => {
@@ -22,6 +23,7 @@ window.addEventListener('click', () => {
 
 let library = [];
 
+//Object constructor:
 function createBook(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -29,13 +31,30 @@ function createBook(title, author, pages, read) {
   this.read = read;
 }
 
+//Object prototype for inheritance:
 createBook.prototype.info = function () {
-  return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+  return [this.title, this.author, this.pages, this.read];
 };
 
+//Function to add objects into an array using the obj constructor:
 function addBookToLibrary(title, author, pages, read) {
   let newObj = new createBook(title, author, pages, read);
   return library.push(newObj);
 }
 
-console.log(library);
+//Iterate on the array and display each book:
+function displayBooks() {
+  library.map((item) => {
+    let elementItem = document.createElement('div');
+    elementItem.classList.add('card');
+    item.info().map((value) => {
+      let innerCardElement = document.createElement('p');
+      innerCardElement.classList.add('card-value');
+      innerCardElement.innerHTML = value;
+      elementItem.appendChild(innerCardElement);
+    });
+    gridContainer.appendChild(elementItem);
+  });
+}
+
+displayBooks();
