30099454c28f874e1918d2c5e526d0292325bbe7
AvihayR <avihayrabi@gmail.com>
2023-03-18 13:13:13 -0700

    Add input fields values render section

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const modal = document.querySelector('.modal');
 const modalBtn = document.getElementById('modal-btn');
 const closeBtn = document.querySelector('.close');
 const gridContainer = document.querySelector('.grid-container');
+const submitBtn = document.getElementById('submit-btn');
+const form = document.querySelector('form');
+let formInputs = form.querySelectorAll('input');
 
 //Modal
 modalBtn.addEventListener('click', () => {
@@ -42,7 +45,7 @@ function addBookToLibrary(title, author, pages, read) {
   return library.push(newObj);
 }
 
-//Iterate on the array and display each book:
+//Iterate on the library array and display each book:
 function displayBooks() {
   library.map((item) => {
     let elementItem = document.createElement('div');
@@ -57,4 +60,19 @@ function displayBooks() {
   });
 }
 
-displayBooks();
+//Render values of form input fields without "Submitting" the form
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  formInputs = Array.from(formInputs).filter((v) => {
+    return v.type !== 'submit';
+  });
+  let formInputValues = formInputs.map((e) => {
+    if (e.type == 'checkbox') {
+      return e.checked;
+    } else {
+      return e.value;
+    }
+  });
+  addBookToLibrary(...formInputValues);
+  displayBooks();
+});
